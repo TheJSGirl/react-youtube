b3f5185fa5084eefeb9d6e3421f6ba1c3c27a354
Priyanka Negi <prinegi100@gmail.com>
2018-03-21 23:34:56 +0530

    -converted functional component to class based component

--- src/components/serachBar.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-const SearchBar = () => {
-  return <input />;
-};
-
-export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import ReactDOM from 'react-dom';
-import React from 'react';
-import SearchBar from './components/serachBar';
+import React, { Component } from 'react';
+import SearchBar from './components/searchBar';
+import YTSearch from 'youtube-api-search';
 const API_KEY = 'AIzaSyAAhcjmGBZ0nd2OZAjshL_Q_DxEI5ZauMA';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+YTSearch({ key: API_KEY, term: 'surfboard' }, data => {
+  console.log(data);
+});
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
